63840eb6b42464a10c30041eef06e136ffc1c1b6
Shiv Charan <shivcharanmt@gmail.com>
2019-08-04 00:32:19 +0530

    Source file for HELLO WORLD PROGRAM in C

---
+++ hello_cmake/main.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+//! main function
+/*!
+  This is main function from where execution of C program start
+ */
+
+int main()
+{      //! printf function
+       /*!
+	 printf function print data on screen.We are printing HELLO WORLD!!! on screen.  
+	*/
+	
+	printf("HELLO WORLD!!!");	
+  	return 0;
+}
